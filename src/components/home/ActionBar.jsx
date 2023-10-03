552c495bfcea822331a8ef5a3ac13de47d47ae8e
ali829 <alielyassraoui@gmail.com>
2023-10-03 17:00:47 +0100

    complete user card ui

--- src/components/home/ActionBar.jsx
+++ src/components/home/ActionBar.jsx
@@ -1,15 +1,17 @@
 // import React from "react"
-import { BiSearchAlt } from 'react-icons/bi';
+import { BiSearchAlt , BiPlus } from 'react-icons/bi';
 const ActionBar = () => {
   return (
     <div className="w-full flex justify-between items-center">
       <div className="w-3/12 px-5 py-2 ring-1 ring-primary-800 rounded-full flex justify-between items-center">
         <input placeholder='Filter Users...' type="text" className="text-primary-50 outline-none bg-transparent border-none w-full"/>
-        <BiSearchAlt className='text-primary-50 text-xl cursor-pointer'/>
+        <BiSearchAlt className='text-primary-50 text-xl cursor-pointer transition-all duration-[0.5s] ease-out hover:scale-110'/>
       </div>
       <div>
-        <button className='outline-none mx-4 text-primary-50 bg-gray-800 py-2 px-8 rounded-full focus:outline-primary-600 focus:outline-dotted focus:outline-offset-2'>Manage</button>
-        <button className='outline-none mx-4 text-primary-50 bg-primary-600 py-2 px-8 rounded-full focus:outline-primary-600 focus:outline-dotted focus:outline-offset-2'>Add User</button>
+        <button className='flex space-y-[30px] items-center transition-all duration-[0.5s] ease-out hover:bg-primary-400 outline-none mx-4 text-primary-50 bg-primary-600 py-2 px-8 rounded-full focus:outline-primary-600 focus:outline-dotted focus:outline-offset-2'>
+          <BiPlus className='mr-3 text-2xl'/>
+            Add User
+          </button>
       </div>
     </div>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,12 @@
 import ActionBar from "../components/home/ActionBar";
+import UserCard from '../components/home/UserCard'
 const home = () => {
   return (
     <>
       <ActionBar/>
+      <div className="grid grid-cols-3 w-full my-16 gap-2">
+        <UserCard/>
+      </div>
     </>
   )
 }
